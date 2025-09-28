198e7d4234d4a64c1bb9d9185f0e41dbfe631b86
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2025-09-28 08:17:19 +0200

    jpa rest

--- cruddemo/src/main/java/com/demo/springboot/cruddemo/dao/IEmployeeRepository.java
+++ cruddemo/src/main/java/com/demo/springboot/cruddemo/dao/IEmployeeRepository.java
@@ -2,6 +2,9 @@ package com.demo.springboot.cruddemo.dao;
 
 import com.demo.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+//@RepositoryRestResource(path="members")
 public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {
 }
